966ef58299a47102a5a0a3299690bf728254a99d
Pierre Abi Chacra <p.abichacra@tecfrac.com>
2020-12-05 11:35:41 +0200

    csv/ & two scatter plots & a script.js

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function showOnlyBlue()
+{
+    $(`[fill='#1f77b4']`).show()
+    $(`[fill='#ff7f0e']`).hide()
+    $(`[fill='#2ca02c']`).hide()
+    $(`[fill='#d62728']`).hide()
+
+}
+function showOnlyOrange()
+{
+    $(`[fill='#ff7f0e']`).show()
+    $(`[fill='#1f77b4']`).hide()
+    $(`[fill='#2ca02c']`).hide()
+    $(`[fill='#d62728']`).hide()
+
+}
+function showOnlyGreen()
+{
+
+    $(`[fill='#2ca02c']`).show()
+    $(`[fill='#d62728']`).hide()
+    $(`[fill='#ff7f0e']`).hide()
+    $(`[fill='#1f77b4']`).hide()
+}
+function showOnlyRed()
+{
+
+    $(`[fill='#d62728']`).show()
+    $(`[fill='#2ca02c']`).hide()
+    $(`[fill='#ff7f0e']`).hide()
+    $(`[fill='#1f77b4']`).hide()
+
+}
